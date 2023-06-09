a4bb818ce6ed1229008eac6ef54f8a4ae66822ba
Sercan Kavdır <sercan.kavdir@maglab.com.tr>
2023-06-09 17:56:28 +0300

    dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sercankavdir/puppy
 
 go 1.20
+
+require github.com/sercankavdir/dog v0.0.0-20230609142023-800e6c750bd1 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/sercankavdir/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func BigBark() {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() {
+	return dog.WhenGrowUp(Barks())
 }
